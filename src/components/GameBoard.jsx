60869147010d4b12dca1c361efb1e5130f2bc62b
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2024-06-30 17:36:48 +0330

    Feat: Add GameBoard and GameOver Components

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-
-const initialGameBoard = [
-  [ null, null, null ],
-  [ null, null, null ],
-  [ null, null, null ]
-];
-export default function GameBoard ( {onSelectSquare, activePlayerSymbol} ) {
-  const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
-
-  function handleSelectSquare ( rowIndex, colIndex ) {
-    setGameBoard(( preGameBoard ) => {
-      const updatedBoard = [ ...preGameBoard.map(innerArray => [ ...innerArray ]) ];
-      updatedBoard[ rowIndex ][ colIndex ] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
-  }
+export default function GameBoard ( {onSelectSquare, board} ) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map(( row, rowIndex ) => <li key={rowIndex}>
+      {board.map(( row, rowIndex ) => <li key={rowIndex}>
         <ol>
           {row.map(( playerSymbol, colIndex ) => <li key={colIndex}>
-            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+            <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                    disabled={playerSymbol !== null}>{playerSymbol} </button>
           </li>)}
         </ol>
       </li>)}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 
-export default function Player ( {initialName, symbol, isActive} ) {
+export default function Player ( {initialName, symbol, isActive, onChangeName} ) {
   const [ isEditing, setIsEditing ] = useState(false);
   const [ playerName, setPlayerName ] = useState(initialName);
 
   function handleEditButton () {
     setIsEditing(( isEditing ) => !isEditing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange ( event ) {
     setPlayerName(event.target.value);
   }
 
-  let editabelPlayerName = <span className="player-name">{playerName}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    editabelPlayerName = <input type="text" value={playerName} onChange={handleChange} required/>;
+    editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required/>;
   }
   return (
     <li className={isActive ? 'active' : undefined}>
                   <span className="player">
-                    {editabelPlayerName}
+                    {editablePlayerName}
                     <span className="player-symbol">{symbol}</span>
                   </span>
       <button onClick={handleEditButton}>{isEditing ? 'Save' : 'Edit'}</button>
